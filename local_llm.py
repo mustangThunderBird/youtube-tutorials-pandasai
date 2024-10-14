e8dbc9dc3710b9b52f49a93a5e564cb42a3f142c
Nicholas Facciola <nicholasfacciola@gmail.com>
2024-10-13 20:49:30 -0700

    connecting to yahoo finance

--- local_llm.py
+++
@@ -1,8 +0,0 @@
-from pandasai import SmartDataframe
-from pandasai.llm.local_llm import LocalLLM
-
-ollama_llm = LocalLLM(api_base="http://localhost:11434/v1", model="mistral-nemo")
-df = SmartDataframe("all_stocks_5yr.csv", config={"llm": ollama_llm})
-
-response = df.chat("What was the average open for INTC in 2016")
-print(response)
